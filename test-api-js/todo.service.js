24257fb6c42b886ad1c4881e5c601f6a56d612be
kogantiaditya <aditya.koganti@slu.edu>
2022-11-01 19:27:38 -0500

    added delete logic in todo.service.js & returning length of current array

--- test-api-js/todo.service.js
+++ test-api-js/todo.service.js
@@ -1,43 +1,49 @@
-class todoservice{
-    todo_data = {
-        "todo":[{
-            "title": "T1",
-            "description": "D1",
-            "done": false
-        },{
-            "title": "T1",
-            "description": "D1",
-            "done": false
-        },{
-            "title": "T1",
-            "description": "D1",
-            "done": false
-        }]
-    }
-    constructor(){
-        this.todos=this.todo_data;
-    }
+class todoservice {
+  todo_data = {
+    todo: [
+      {
+        title: "T1",
+        description: "D1",
+        done: false,
+      },
+      {
+        title: "T1",
+        description: "D1",
+        done: false,
+      },
+      {
+        title: "T1",
+        description: "D1",
+        done: false,
+      },
+    ],
+  };
+  constructor() {
+    this.todos = this.todo_data;
+  }
 
-    get_todos(){
-        return this.todos;
-    }
+  get_todos() {
+    return this.todos;
+  }
 
-    add_todo(todo){
-        // Your code here
-        this.todos.todo.push(todo);
-        console.log("appended todo: " + todo);
-        console.log("updated todos- " + this.todos.todo);
-        return this.todos.todo.length
-    }
+  add_todo(todo) {
+    this.todos.todo.push(todo);
+    console.log("appended todo: " + todo);
+    console.log("updated todos- " + this.todos.todo);
+    return this.todos.todo.length;
+  }
 
-    delete_todo(id){
-        // Your code here
+  delete_todo(id) {
+    let index = this.todos.todo.indexOf(id)
+    if(index>-1){
+        this.todos.todo.splice(index, 1);
     }
+    return this.todos.todo.length;
+  }
 
-    update_todo(id, todo){
-        // Your code here
-    }
+  update_todo(id, todo) {
+    // Your code here
+  }
 }
 
-
-module.exports= todoservice;
+module.exports = todoservice;
